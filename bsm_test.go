df1119ecff273e4f51949300259534ed1498d6d2
Andres Orozco <ao15@my.fsu.edu>
2018-11-19 13:29:13 -0500

    test case added

--- bsm_test.go
+++ bsm_test.go
@@ -1,7 +1,28 @@
 package bsm
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+	"time"
+)
 
-func BSM_Test(t *testing.T) {
+// BSMTest test for option price
+func TestBSM(t *testing.T) {
+
+	//Test option on AAPL with following data:
+	// Current price: 186.28
+	// Exp Date: 11/30/18
+	// Strike Price: 190
+	// Vol: 34.4
+	// Call price: 2.90-2.93 (spread)
+	date := time.Date(2018, time.November, 30, 0, 0, 0, 0, time.UTC)
+	opt := New(186.28, 190.0, 34.4, true, "AAPL", date)
+
+	price := opt.Value()
+	fmt.Println(price)
+	if price != 2.90 {
+		t.Logf("Call Price: %f", price)
+		t.Error("Option returns wrong value")
+	}
 
 }
